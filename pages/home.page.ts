f28f17a4369930c93d407b74e7e2a65d7ea3ba4d
Raj Kumar <r.rajkumar1514@gmail.com>
2025-08-13 13:08:52 +0530

    updated yml

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,18 +1,14 @@
 import { Page, Locator } from '@playwright/test';
 
 export class HomePage {
-
-    private demoSitesMenu: Locator;
-    private practiceAutomationMenu: Locator;
-    private registrationFormLink: Locator;
+  private demoSitesMenu: Locator;
+  private practiceAutomationMenu: Locator;
+  private registrationFormLink: Locator;
 
   constructor(private readonly page: Page) {
-    this.page = page;
     this.demoSitesMenu = page.locator('(//a[contains(text(),"Demo Sites")])[2]');
     this.practiceAutomationMenu = page.locator('(//a[contains(text(),"Practice Automation")])[2]');
     this.registrationFormLink = page.locator('(//a[normalize-space()="Registration Form"])[2]');
-    
-
   }
 
   async navigateToHome() {
@@ -20,13 +16,13 @@ export class HomePage {
   }
 
   async navigateToRegistrationForm() {
+    await this.demoSitesMenu.waitFor({ state: 'visible' });
     await this.demoSitesMenu.hover();
+
+    await this.practiceAutomationMenu.waitFor({ state: 'visible' });
     await this.practiceAutomationMenu.hover();
+
     await this.registrationFormLink.waitFor({ state: 'visible' });
-    await this.registrationFormLink.click();
-
-
-
-   
+    await this.registrationFormLink.click({ timeout: 10000 }); // CI safety
   }
 }
